a328c3fa838ba60aa4bc68b7c1b2afe32add642a
NicolasGula <nicolasgula@outlook.com>
2022-03-28 17:47:01 -0300

    update titel

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,16 +1,10 @@
 import ContainerCards from "./ContainerCards"
 
-const title = {
-  title: "Trabajos",
-  subtitle: "25 de Marzo de 2022 - 6:22 AM"
-}
-
 function Works() {
   return (
     <div className="displaySection adjustPage">
       <div className='containerTitleBlack'>
-        <h1 id='title'>{title.title}</h1>
-        <h2 id='subTitle'>{title.subtitle}</h2>
+        <h1 id='title'>"Trabajos"</h1>
       </div>
       <ContainerCards />
     </div>
